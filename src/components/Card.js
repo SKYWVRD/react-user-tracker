2cb5c71e6dca6563c6256fd4ffd78ecfae9a39fd
Sean Boonzaier <boonzaiersa@gmail.com>
2022-11-21 21:47:41 +0200

    Create reusable card component

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import react from 'react'
+import styles from './Card.module.css'
+
+const Card = (props) => {
+
+    const classes = "card " + props.className
+
+    return (
+        <div className={classes}>{props.children}</div>
+    )
+}
+
+export default Card;
