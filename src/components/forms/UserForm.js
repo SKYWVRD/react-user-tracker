5b09895a4b605f46df5f9770a294b7c51df1901b
Sean Boonzaier <boonzaiersa@gmail.com>
2022-11-22 21:56:59 +0200

    Add username and userage state handling to form

--- src/components/forms/UserForm.js
+++ src/components/forms/UserForm.js
@@ -1,20 +1,33 @@
-import react from 'react'
-import Card from '../Card'
+import react, { useState } from "react";
+import Card from "../Card";
 
 const UserForm = () => {
+  const [userName, setUsername] = useState("");
+  const [userAge, setUserAge] = useState(18);
 
+  const userNameChangeHandler = (event) => {
+    setUsername(event.target.value);
+  };
 
-    return (
-        <Card>
-            <form>
-                <label htmlFor="username">Username</label>
-                <input type='text'></input>
-                <label htmlFor='age'>Age</label>
-                <input type='number' value='18'></input>
-                <input type='submit'></input>
-            </form>
-        </Card>
-    )
-}
+  const ageChangeHandler = (event) => {
+    setUserAge(event.target.value);
+  };
+
+  return (
+    <Card>
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text" onChange={userNameChangeHandler}></input>
+        <label htmlFor="age">Age</label>
+        <input
+          type="number"
+          defaultValue={userAge}
+          onChange={ageChangeHandler}
+        ></input>
+        <input type="submit" className="submit-button"></input>
+      </form>
+    </Card>
+  );
+};
 
 export default UserForm;
