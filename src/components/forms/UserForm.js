f0aad7903aa202dda3ba582252b869cf19bef651
Sean Boonzaier <boonzaiersa@gmail.com>
2022-11-22 21:44:18 +0200

    Add basic layout of user input form

--- src/components/forms/UserForm.js
+++ src/components/forms/UserForm.js
@@ -6,6 +6,13 @@ const UserForm = () => {
 
     return (
         <Card>
+            <form>
+                <label htmlFor="username">Username</label>
+                <input type='text'></input>
+                <label htmlFor='age'>Age</label>
+                <input type='number' value='18'></input>
+                <input type='submit'></input>
+            </form>
         </Card>
     )
 }
